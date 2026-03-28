edeed42a7233c2a6f4722134bb00e87dc3a742a5
mshafaie8 <mshafaie@berkeley.edu>
2026-03-28 11:42:22 -0700

    init BasicTokenizer

---
+++ tokenizer.py
@@ -0,0 +1,12 @@
+"""Class with BasicTokenizer implementation."""
+
+class BasicTokenizer:
+
+    def train(self, text, vocab_size, verbose=False):
+        pass
+
+    def encode(self, text):
+        pass
+
+    def decode(self, ids):
+        pass
